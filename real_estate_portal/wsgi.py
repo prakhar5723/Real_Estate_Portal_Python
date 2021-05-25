7a6cd3d3502564563aba8a1d96a627100f8421e2
prakhar5723 <79194076+prakhar5723@users.noreply.github.com>
2021-05-25 21:53:24 +0530

    mend

--- real_estate_portal/wsgi.py
+++ real_estate_portal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for btre project.
+WSGI config for Real Estate Portal.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'btre.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_estate_portal.settings')
 
 application = get_wsgi_application()
